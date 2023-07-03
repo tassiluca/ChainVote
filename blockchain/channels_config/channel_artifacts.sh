52ea1d02b55562273dd1ac48f963e7ace2fdb0dc
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-07-03 21:19:42 +0200

    feat: add partial scripts to download binaries + bring up the network (wip)

--- blockchain/channels_config/channel_artifacts.sh
+++ blockchain/channels_config/channel_artifacts.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-
-# Change to your path
-BIN_PATH=~/fabric-samples/bin
-OUTPUT_PATH=/tmp/hyperledger
-
+#
+# Create and join channel script
 
 # Genesis block
-$BIN_PATH/configtxgen -profile OrgsOrdererGenesis -outputBlock $OUTPUT_PATH/org0/artifacts/genesis.block -channelID syschannel
+configtxgen -profile OrgsOrdererGenesis -outputBlock /tmp/hyperledger/org0/artifacts/genesis.block -channelID syschannel
 
 # Channel 1 transactions and anchors
-$BIN_PATH/configtxgen -profile OrgsChannel1 -outputCreateChannelTx  $OUTPUT_PATH/org1/artifacts/channel.tx -channelID ch1
-$BIN_PATH/configtxgen -profile OrgsChannel1 -outputAnchorPeersUpdate $OUTPUT_PATH/org1/artifacts/org1MSPanchors.tx -channelID ch1 -asOrg org1MSP
+configtxgen -profile OrgsChannel1 -outputCreateChannelTx /tmp/hyperledger/org1/artifacts/channel.tx -channelID ch1
+configtxgen -profile OrgsChannel1 -outputAnchorPeersUpdate /tmp/hyperledger/org1/artifacts/org1MSPanchors.tx -channelID ch1 -asOrg org1MSP
 
 # Channel 2 transactions and anchors
-$BIN_PATH/configtxgen -profile OrgsChannel2 -outputCreateChannelTx  $OUTPUT_PATH/org2/artifacts/channel.tx -channelID ch2
-$BIN_PATH/configtxgen -profile OrgsChannel2 -outputAnchorPeersUpdate $OUTPUT_PATH/org2/artifacts/org2MSPanchors.tx -channelID ch2 -asOrg org2MSP
+configtxgen -profile OrgsChannel2 -outputCreateChannelTx /tmp/hyperledger/org2/artifacts/channel.tx -channelID ch2
+configtxgen -profile OrgsChannel2 -outputAnchorPeersUpdate /tmp/hyperledger/org2/artifacts/org2MSPanchors.tx -channelID ch2 -asOrg org2MSP
 
+# now this works...
+# peer channel create -c ch1 -f /tmp/hyperledger/org1/artifacts/channel.tx -o localhost:7050 --ordererTLSHostnameOverride orderer1-org0 --outputBlock /tmp/hyperledger/org1/peer1/assets/mychannel.block --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
---
+++ blockchain/network.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [[ $# != 1 || ($1 != "up" && $1 != "down") ]]; then
+    echo "Usage: ./network [up|down]"
+    echo "Description:"
+    echo "  Script used to easily bring up and down the network."
+    echo 
+    echo "Options:"
+    echo "  up  - Bring up the network"
+    echo "  down - Bring down the network"
+    exit 1
+fi;
+
+function upNetwork() {
+    echo "Setup binaries"
+    if [[ ! -d ./bin/ ]]; then
+        ./install-binaries.sh
+    fi;
+    export PATH="$PATH:$PWD/bin"
+    echo "Up ca-tls rca-org0 rca-org1 rca-org2"
+    docker-compose up -d ca-tls rca-org0 rca-org1 rca-org2
+    sleep 5
+    echo "Enrol registrar of each CA and register all entities"
+    ./reg.sh
+    echo "Enrol entities for each organization"
+    ./enroll.sh
+}
+
+function downNetwork() {
+    echo "Downing network..."
+    docker-compose down
+    echo "Delete /tmp/hyperledger/..."
+    rm -rf /tmp/hyperledger
+    echo "Done."
+}
+
+if [[ $1 == "up" ]]; then
+    upNetwork
+elif [[ $1 == "down" ]]; then
+    downNetwork
+fi;
