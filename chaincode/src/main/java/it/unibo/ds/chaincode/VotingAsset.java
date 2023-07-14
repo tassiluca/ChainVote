5cb52c057952a1d07eba7536c62ebb0750d00440
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-07-14 08:17:36 +0200

    docs: add doc comments

--- chaincode/src/main/java/it/unibo/ds/chaincode/VotingAsset.java
+++ chaincode/src/main/java/it/unibo/ds/chaincode/VotingAsset.java
@@ -5,10 +5,18 @@ import it.unibo.ds.core.Voting;
 import org.hyperledger.fabric.contract.annotation.DataType;
 import org.hyperledger.fabric.contract.annotation.Property;
 
+/**
+ * The hyperledger fabric notion of voting asset.
+ * @param asset the voting asset.
+ */
 @DataType
 public record VotingAsset(@Property Voting asset) {
 
-    public VotingAsset(@JsonProperty("asset") Voting asset) {
+    /**
+     * Creates an asset with the given voting.
+     * @param asset the concrete voting.
+     */
+    public VotingAsset(final @JsonProperty("asset") Voting asset) {
         this.asset = asset;
     }
 }
