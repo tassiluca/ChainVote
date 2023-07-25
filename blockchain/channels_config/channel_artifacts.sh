6e2c4d9779f2e323e19e4ee5ad34ebf0a896cb3b
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-07-25 15:24:18 +0200

    refactor: remove reduntant export declaration, improve readability and add some comments

--- blockchain/channels_config/channel_artifacts.sh
+++ blockchain/channels_config/channel_artifacts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Create and join channel script
+# Script to create channel artifacts.
 
 # Genesis block
 configtxgen -profile OrgsOrdererGenesis -outputBlock /tmp/hyperledger/org0/artifacts/genesis.block -channelID syschannel
@@ -12,6 +12,3 @@ configtxgen -profile OrgsChannel1 -outputAnchorPeersUpdate /tmp/hyperledger/org1
 # Channel 2 transactions and anchors
 configtxgen -profile OrgsChannel2 -outputCreateChannelTx /tmp/hyperledger/org2/artifacts/channel.tx -channelID ch2
 configtxgen -profile OrgsChannel2 -outputAnchorPeersUpdate /tmp/hyperledger/org2/artifacts/org2MSPanchors.tx -channelID ch2 -asOrg org2MSP
-
-# now this works...
-#peer channel create -c ch1 -f /tmp/hyperledger/org1/artifacts/channel.tx -o localhost:7050 --ordererTLSHostnameOverride orderer1-org0 --outputBlock /tmp/hyperledger/org1/peer1/assets/mychannel.block --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
