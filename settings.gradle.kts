6b657f8cbec3c3a72387a6d02518a08e903d4e14
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-07-27 20:44:45 +0200

    build: add git hook to exec check before commit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,16 @@
+plugins {
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "1.1.9"
+}
+
+gitHooks {
+    preCommit {
+        tasks("check")
+    }
+    createHooks()
+}
+
 rootProject.name = "chain-vote"
+
 include(
     "core",
     "presentation",
