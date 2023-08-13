63690c3efc307e05ea8cae10d59e9ddea5ff065f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-08-13 16:32:18 +0200

    style: remove commented line no more useful

--- blockchain/channels_config/channel_creation.sh
+++ blockchain/channels_config/channel_creation.sh
@@ -7,12 +7,6 @@ export FABRIC_CFG_PATH=$PWD/org1
 
 # Org1
 echo "Creating ch1 and join peer1-org1 and peer2-org1 to it"
-
-# export CORE_PEER_LOCALMSPID="org1MSP"
-# export CORE_PEER_TLS_ROOTCERT_FILE=/tmp/hyperledger/org1/peer1/assets/tls-ca/tls-ca-cert.pem
-# export CORE_PEER_MSPCONFIGPATH=/tmp/hyperledger/org1/admin/msp
-# export CORE_PEER_ADDRESS=localhost:7051
-
 peer channel create --channelID ch1 --file /tmp/hyperledger/org1/artifacts/channel.tx --orderer localhost:7050 --ordererTLSHostnameOverride orderer1-org0 --outputBlock /tmp/hyperledger/org1/peer1/assets/ch1.block --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
 # join peer1-org1
 peer channel join --blockpath /tmp/hyperledger/org1/peer1/assets/ch1.block
@@ -27,17 +21,11 @@ CORE_PEER_ADDRESS=localhost:8051 peer channel getinfo --channelID ch1
 sleep 3
 
 # Org2
-echo "Creating ch2 and join peer1-org2 and peer2-org2 to it"
 export FABRIC_CFG_PATH=$PWD/org2
 
-# export CORE_PEER_LOCALMSPID="org2MSP"
-# export CORE_PEER_TLS_ROOTCERT_FILE=/tmp/hyperledger/org2/peer1/assets/tls-ca/tls-ca-cert.pem
-# export CORE_PEER_MSPCONFIGPATH=/tmp/hyperledger/org2/admin/msp
-# export CORE_PEER_ADDRESS=localhost:9051
-# export CORE_PEER_GOSSIP_BOOTSTRAP=localhost:9051
-
+echo "Creating ch2 and join peer1-org2 and peer2-org2 to it"
 peer channel create --channelID ch2 --file /tmp/hyperledger/org2/artifacts/channel.tx --orderer localhost:7050 --ordererTLSHostnameOverride orderer1-org0 --outputBlock /tmp/hyperledger/org2/peer1/assets/ch2.block --tls --cafile /tmp/hyperledger/org2/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
-# join peer1-org2
+# join peer1-org2
 peer channel join --blockpath /tmp/hyperledger/org2/peer1/assets/ch2.block
 # join peer2-org2
 CORE_PEER_ADDRESS=localhost:10051 peer channel join --blockpath /tmp/hyperledger/org2/peer1/assets/ch2.block
