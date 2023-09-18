b8bcacd33d66442a95c4de7dfca988bcade8b9cb
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-09-18 18:37:41 +0200

    refactor: remove no more useful scripts

--- blockchain/channels_config/channel_creation.sh
+++ blockchain/channels_config/channel_creation.sh
@@ -10,9 +10,9 @@ export FABRIC_CFG_PATH=$PWD/org1
 # Org1
 echo "Creating ch1 and join peer1-org1 and peer2-org1 to it"
 peer channel create --channelID ch1 --file /tmp/hyperledger/org1/artifacts/channel.tx --orderer localhost:7050 --ordererTLSHostnameOverride orderer1-org0 --outputBlock /tmp/hyperledger/org1/peer1/assets/ch1.block --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
-# join peer1-org1
+# join peer1-org1 to ch1
 peer channel join --blockpath /tmp/hyperledger/org1/peer1/assets/ch1.block
-# join peer2-org1
+# join peer2-org1 to ch1
 CORE_PEER_ADDRESS=localhost:8051 peer channel join --blockpath /tmp/hyperledger/org1/peer1/assets/ch1.block
 # update anchor peer
 peer channel update --channelID ch1 --file /tmp/hyperledger/org1/artifacts/org1MSPanchors.tx --orderer localhost:7050 --ordererTLSHostnameOverride orderer1-org0 --tls --cafile /tmp/hyperledger/org1/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
@@ -25,13 +25,13 @@ sleep 3
 # Org2
 export FABRIC_CFG_PATH=$PWD/org2
 
-echo "Creating ch2 and join peer1-org2 and peer2-org2 to it"
+echo "Creating ch2 and join peer1-org2 and peer2-org2 to ch1+ch2"
 cp /tmp/hyperledger/org1/peer1/assets/ch1.block /tmp/hyperledger/org2/peer1/assets/
 peer channel create --channelID ch2 --file /tmp/hyperledger/org2/artifacts/channel.tx --orderer localhost:7050 --ordererTLSHostnameOverride orderer1-org0 --outputBlock /tmp/hyperledger/org2/peer1/assets/ch2.block --tls --cafile /tmp/hyperledger/org2/peer1/tls-msp/tlscacerts/tls-0-0-0-0-7052.pem
-# join peer1-org2
+# join peer1-org2 to both ch1 and ch2
 peer channel join --blockpath /tmp/hyperledger/org2/peer1/assets/ch2.block
 peer channel join --blockpath /tmp/hyperledger/org2/peer1/assets/ch1.block
-# join peer2-org2
+# join peer2-org2 to both ch1 and ch2
 CORE_PEER_ADDRESS=localhost:10051 peer channel join --blockpath /tmp/hyperledger/org2/peer1/assets/ch2.block
 CORE_PEER_ADDRESS=localhost:10051 peer channel join --blockpath /tmp/hyperledger/org2/peer1/assets/ch1.block
 # update anchor peer
