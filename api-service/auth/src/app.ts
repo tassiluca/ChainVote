37a714bb93f76c5a19f70daa0382611d40d2ce7f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-10-02 22:54:05 +0200

    chore: removed api from chaincode branch

--- api-service/auth/src/app.ts
+++
@@ -1,7 +0,0 @@
-import ExpressConfig from "./configs/express.config";
-
-const app = ExpressConfig();
-const PORT = process.env.PORT || 8180;
-
-app.listen(PORT, () => console.log("Server Running on Port " + PORT));
-export default app;
