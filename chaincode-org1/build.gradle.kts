4fd47fd736af7a6f69d75b911c2fa612b4a4658f
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-10-03 11:29:54 +0200

    build(chaincode): remove jackson dependencies

--- chaincode-org1/build.gradle.kts
+++ chaincode-org1/build.gradle.kts
@@ -17,8 +17,6 @@ dependencies {
     implementation(libs.fabric.chaincode.shim)
     implementation(libs.genson)
     implementation(libs.json)
-    implementation("com.fasterxml.jackson.jr:jackson-jr-annotation-support:2.15.1")
-    implementation("com.fasterxml.jackson.jr:jackson-jr-annotation-support:2.15.1")
     testImplementation(libs.assertj.core)
     testImplementation(libs.mockito.core)
 }
--- chaincode-org2/build.gradle.kts
+++ chaincode-org2/build.gradle.kts
@@ -14,12 +14,11 @@ repositories {
 
 dependencies {
     api(project(":chaincode"))
+    api(project(":chaincode-org1"))
     implementation(libs.fabric.chaincode.shim)
     implementation(libs.genson)
     implementation(libs.json)
     implementation(libs.commons.lang)
-    implementation("com.fasterxml.jackson.core:jackson-annotations:2.3.0")
-    implementation(project(mapOf("path" to ":chaincode-org1")))
     testImplementation(libs.assertj.core)
     testImplementation(libs.mockito.core)
 }
