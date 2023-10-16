f56c3364ec851a035691cf5cf7a537aac776f38a
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-16 17:01:52 +0200

    fix: solved problem of connection with the smart contract

--- api-service/api/src/blockchain/gateway.ts
+++ api-service/api/src/blockchain/gateway.ts
@@ -1,9 +1,9 @@
-import * as grpc from '@grpc/grpc-js';
-import * as crypto from 'crypto';
-import { connect, Identity, signers } from '@hyperledger/fabric-gateway';
-import { promises as fs } from 'fs';
-import { TextDecoder } from 'util';
+
 import path from "path";
+import { promises as fs } from 'fs';
+import * as grpc from '@grpc/grpc-js';
+import {connect, Identity, Signer, signers} from "@hyperledger/fabric-gateway";
+import * as crypto from 'crypto';
 
 const utf8Decoder = new TextDecoder();
 
@@ -14,31 +14,92 @@ async function main(): Promise<void> {
         "hyperledger",
         "org1"
     );
-    
 
-    const credentials = await fs.readFile('path/to/certificate.pem');
-    const identity: Identity = { mspId: 'myorg', credentials };
+    const keyDirectoryPathOrg1 = path.resolve(
+        org1Dir,
+        'client',
+        'msp',
+        'keystore'
+    );
 
-    const privateKeyPem = await fs.readFile('path/to/privateKey.pem');
-    const privateKey = crypto.createPrivateKey(privateKeyPem);
-    const signer = signers.newPrivateKeySigner(privateKey);
+    const certPathOrg1 = path.resolve(
+        org1Dir,
+        'client',
+        'msp',
+        'signcerts',
+        'cert.pem'
+    );
 
-    const client = new grpc.Client('gateway.example.org:1337', grpc.credentials.createInsecure());
+    const tlsCertPathOrg1 = path.resolve(
+        org1Dir,
+        'client',
+        'tls-msp',
+        'tlscacerts',
+        'tls-0-0-0-0-7052.pem'
+    );
 
-    const gateway = connect({ identity, signer, client });
+    const peerEndpointOrg1 = 'localhost:7051';
+    const peerNameOrg1 = 'peer1-org1';
+    const channelName = 'ch1';
+    const chaincodeName = 'chaincode-org1';
+    const mspIdOrg1 = 'org1MSP';
+
+    async function newGrpcConnection(
+        tlsCertPath: string,
+        peerEndpoint: string,
+        peerName: string
+    ): Promise<grpc.Client> {
+        const tlsRootCert = await fs.readFile(tlsCertPath);
+        const tlsCredentials = grpc.credentials.createSsl(tlsRootCert);
+        return new grpc.Client(peerEndpoint, tlsCredentials, {
+            'grpc.ssl_target_name_override': peerName,
+        });
+    }
+
+    async function newIdentity(
+        certPath: string,
+        mspId: string
+    ): Promise<Identity> {
+        const credentials = await fs.readFile(certPath);
+        return { mspId, credentials };
+    }
+
+    async function newSigner(keyDirectoryPath: string): Promise<Signer> {
+        const files = await fs.readdir(keyDirectoryPath);
+        const keyPath = path.resolve(keyDirectoryPath, files[0]);
+        const privateKeyPem = await fs.readFile(keyPath);
+        const privateKey = crypto.createPrivateKey(privateKeyPem);
+        return signers.newPrivateKeySigner(privateKey);
+    }
+
+
+    const clientOrg1 = await newGrpcConnection(
+        tlsCertPathOrg1,
+        peerEndpointOrg1,
+        peerNameOrg1
+    );
+
+    const gatewayOrg1 = connect({
+        client: clientOrg1,
+        identity: await newIdentity(certPathOrg1, mspIdOrg1),
+        signer: await newSigner(keyDirectoryPathOrg1),
+    });
+
+
     try {
-        const network = gateway.getNetwork('channelName');
-        const contract = network.getContract('chaincodeName');
+        const contractOrg1 = gatewayOrg1
+            .getNetwork(channelName)
+            .getContract(chaincodeName);
 
-        const putResult = await contract.submitTransaction('put', 'time', new Date().toISOString());
-        console.log('Put result:', utf8Decoder.decode(putResult));
+        const resultBytes = await contractOrg1.evaluateTransaction('getAllAssets');
 
-        const getResult = await contract.evaluateTransaction('get', 'time');
-        console.log('Get result:', utf8Decoder.decode(getResult));
+        const resultString = utf8Decoder.decode(resultBytes);
+        const result = JSON.parse(resultString);
+        console.log('*** Result:', result);
+
     } finally {
-        gateway.close();
-        client.close()
+        gatewayOrg1.close();
+        clientOrg1.close();
     }
 }
-
-main().catch(console.error);
+main().then(() => console.log('done')).catch((e) => console.log(e));
