f6a894c109fb088fd28a7213bc46865eebec6f05
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-20 19:30:53 +0200

    feat: Add endpoints to the router file

--- api-service/api/src/routes/codes.route.ts
+++ api-service/api/src/routes/codes.route.ts
@@ -1,9 +1,17 @@
 import { Router } from "express";
+import {generateFor, invalidate, isValid, verifyCodeOwner} from "../controllers/codes";
 
 const codesRoute =  Router();
 
+codesRoute.post("/generate", generateFor);
 
 
+codesRoute.post("/isValid", isValid);
 
+
+codesRoute.put("/invalidate", invalidate);
+
+
+codesRoute.post("/verifyOwner", verifyCodeOwner);
 
 export default codesRoute;
