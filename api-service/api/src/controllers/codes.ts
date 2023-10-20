eae2fbb9c030fcec232ff286036183dbbe145fd1
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-20 19:01:42 +0200

    feat(api): add generateFor invocation in codes controller

--- api-service/api/src/controllers/codes.ts
+++ api-service/api/src/controllers/codes.ts
@@ -1,8 +1,33 @@
-//import {NextFunction, Request, Response} from "express";
-//import {Contract, Gateway, Network} from "@hyperledger/fabric-gateway";
-//import GrpcClientPool from "../blockchain/grpc.client.pool";
-//import {Org2Peer} from "../blockchain/peer.enum";
+import {NextFunction, Request, Response} from "express";
+import {Contract, Gateway, Network} from "@hyperledger/fabric-gateway";
+import GrpcClientPool from "../blockchain/grpc.client.pool";
+
+import {Org2Peer} from "../blockchain/peer.enum";
+import {StatusCodes} from "http-status-codes";
+
+const channelName = "ch2";
+const contractName = "chaincode-org2";
+const utf8Decoder = new TextDecoder();
+
+export async function generateFor(req: Request, res: Response, next: NextFunction) {
+    try {
+        const gatewayOrg1: Gateway = await GrpcClientPool.getInstance().getClientForPeer(Org2Peer.PEER1);
+        const network: Network = gatewayOrg1.getNetwork(channelName);
+        const contract: Contract = network.getContract(contractName);
+
+        const userId = req.body.userId;
+        const codeRequest: Uint8Array = await contract.submit('CodesManagerContract:generateFor', {
+            arguments: [`userId:${userId}`],
+            transientData: {
+                "userId": userId,
+            }
+        });
+
+        const resultJson = utf8Decoder.decode(codeRequest);
+        return res.status(StatusCodes.OK).send(JSON.parse(resultJson));
+    } catch (error) {
+        return next(error)
+    }
+}
 
 
-// const channelName = "ch2";
-// const contractName = "chaincode-org2";
