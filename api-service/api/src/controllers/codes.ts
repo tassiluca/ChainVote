975b9e81307d12bbbf986ed934a636f91ddbdc9d
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-20 19:15:45 +0200

    feat: add controller functions for isValid and invalidate functions of codes contract

--- api-service/api/src/controllers/codes.ts
+++ api-service/api/src/controllers/codes.ts
@@ -9,6 +9,12 @@ const channelName = "ch2";
 const contractName = "chaincode-org2";
 const utf8Decoder = new TextDecoder();
 
+/**
+ * Generate a code that can be used by an user for voting
+ * @param req
+ * @param res
+ * @param next
+ */
 export async function generateFor(req: Request, res: Response, next: NextFunction) {
     try {
         const gatewayOrg1: Gateway = await GrpcClientPool.getInstance().getClientForPeer(Org2Peer.PEER1);
@@ -30,4 +36,60 @@ export async function generateFor(req: Request, res: Response, next: NextFunctio
     }
 }
 
+/**
+ * Check if a code is valid
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function isValid(req: Request, res: Response, next: NextFunction) {
+    try {
+        const gatewayOrg1: Gateway = await GrpcClientPool.getInstance().getClientForPeer(Org2Peer.PEER1);
+        const network: Network = gatewayOrg1.getNetwork(channelName);
+        const contract: Contract = network.getContract(contractName);
 
+        const code = req.body.code;
+        const userId = req.body.userId;
+
+        const codeRequest: Uint8Array = await contract.evaluate('CodesManagerContract:isValid', {
+            transientData: {
+                "code": code,
+                "userId": userId
+            }
+        });
+
+        const resultJson = utf8Decoder.decode(codeRequest);
+        return res.status(StatusCodes.OK).send(JSON.parse(resultJson));
+    } catch (error) {
+        return next(error)
+    }
+}
+
+/**
+ * Invalidate a code
+ * @param req
+ * @param res
+ * @param next
+ */
+export async function invalidate(req: Request, res: Response, next: NextFunction) {
+    try {
+        const gatewayOrg1: Gateway = await GrpcClientPool.getInstance().getClientForPeer(Org2Peer.PEER1);
+        const network: Network = gatewayOrg1.getNetwork(channelName);
+        const contract: Contract = network.getContract(contractName);
+
+        const code = req.body.code;
+        const userId = req.body.userId;
+
+        const codeRequest: Uint8Array = await contract.submit('CodesManagerContract:invalidate', {
+            transientData: {
+                "code": code,
+                "userId": userId
+            }
+        });
+
+        const resultJson = utf8Decoder.decode(codeRequest);
+        return res.status(StatusCodes.OK).send(JSON.parse(resultJson));
+    } catch (error) {
+        return next(error)
+    }
+}
