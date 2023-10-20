b5d12d1ade622c3048889531d38736333c187ebe
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-20 18:31:24 +0200

    feat(api): initialize the router and controller for codes-related chaincode invocations

---
+++ api-service/api/src/controllers/codes.ts
@@ -0,0 +1,8 @@
+//import {NextFunction, Request, Response} from "express";
+//import {Contract, Gateway, Network} from "@hyperledger/fabric-gateway";
+//import GrpcClientPool from "../blockchain/grpc.client.pool";
+//import {Org2Peer} from "../blockchain/peer.enum";
+
+
+// const channelName = "ch2";
+// const contractName = "chaincode-org2";
---
+++ api-service/api/src/routes/codes.route.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const codesRoute =  Router();
+
+
+
+
+
+export default codesRoute;
