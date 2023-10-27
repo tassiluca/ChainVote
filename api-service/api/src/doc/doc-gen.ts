d274b830ba6b3f1bbcd974e3c716c16852162e30
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-10-27 21:00:27 +0200

    feat(api): setted up the API documentation generator

---
+++ api-service/api/src/doc/doc-gen.ts
@@ -0,0 +1,19 @@
+import swaggerJSDoc from 'swagger-jsdoc';
+import fs from 'fs';
+
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Chainvote API documentation',
+            version: '1.0.0',
+        },
+    },
+    apis: ['./src/routes/*.ts'],
+};
+
+const openapiSpecification = swaggerJSDoc(options);
+fs.writeFileSync('swagger.json', JSON.stringify(openapiSpecification, null, 2));
+
+export default openapiSpecification;
