d4abe48279fc51e8502fe8a4835cd68a184d1040
Luca Tassinari <luca.tassinari.2000@gmail.com>
2023-11-01 11:36:46 +0100

    docs: add missing docs

--- smart-contracts/presentation/src/main/java/it/unibo/ds/chainvote/SerializerCustomUtils.java
+++ smart-contracts/presentation/src/main/java/it/unibo/ds/chainvote/SerializerCustomUtils.java
@@ -4,10 +4,19 @@ import it.unibo.ds.chainvote.assets.Election;
 import it.unibo.ds.chainvote.assets.ElectionInfo;
 import it.unibo.ds.chainvote.utils.Utils;
 
+/**
+ * TODO to document
+ */
 public final class SerializerCustomUtils {
 
     private SerializerCustomUtils() { }
 
+    /**
+     * TODO to document (now placeholder just to generate javadoc)
+     * @param object the election
+     * @param info the info
+     * @return a string
+     */
     public static String serializeForAffluence(final Election object, final ElectionInfo info) {
         double affluence = (double) object.getBallots().size() * 100 / (double) info.getVotersNumber();
         return "{\"id\":\"" + Utils.calculateID(info) + "\",\"affluence\":\"" + affluence +  "%\"}";
