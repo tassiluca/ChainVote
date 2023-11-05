aa6bcd02c0623a457cb1bfb54ed3f470f8f6d5a5
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-11-05 12:18:40 +0100

    refactor(api): applied api rate limiter to the election routes

--- api-service/api/src/routes/election.route.ts
+++ api-service/api/src/routes/election.route.ts
@@ -1,7 +1,43 @@
 import { Router } from "express";
 import {castVote, createElection, deleteElection, getAllElection, readElection} from "../controllers/election";
+import RedisLimiterStorage from "../configs/redis.config";
+import {apiLimiter, ApiLimiterEntry} from "core-components";
 
 const electionRouter = Router();
+
+const limitStorage = new RedisLimiterStorage();
+const API_LIMITER_RULES: ApiLimiterEntry = {
+    "/all": {
+        "GET": {
+            time: 10,
+            limit: 10
+        }
+    },
+    "/detail": {
+        "GET": {
+            time: 20,
+            limit: 40
+        }
+    },
+    "/vote": {
+        "PUT": {
+            time: 20,
+            limit: 5
+        }
+    },
+    "/": {
+        "POST": {
+            time: 20,
+            limit: 5
+        },
+        "DELETE": {
+            time: 20,
+            limit: 5
+        }
+    }
+}
+
+electionRouter.use(apiLimiter(API_LIMITER_RULES, limitStorage));
 
 electionRouter.get("/all", getAllElection);
 
@@ -9,7 +45,7 @@ electionRouter.get("/detail/:electionId", readElection);
 
 electionRouter.post("/", createElection);
 
-electionRouter.put("/:electionId/vote", castVote);
+electionRouter.put("/vote/:electionId", castVote);
 
 electionRouter.delete("/",  deleteElection);
 
