03d8f8c3ad446b85c56e81fcdf41fad431023f52
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-11-05 12:30:19 +0100

    refactor(api): applied api rate limiter to the codes routes

--- api-service/api/src/routes/codes.route.ts
+++ api-service/api/src/routes/codes.route.ts
@@ -1,7 +1,42 @@
 import { Router } from "express";
 import {generateCodeFor, invalidate, isValid, verifyCodeOwner} from "../controllers/codes";
+import RedisLimiterStorage from "../configs/redis.config";
+import {apiLimiter, ApiLimiterEntry} from "core-components";
 
 const codesRoute =  Router();
+
+const limitStorage = new RedisLimiterStorage();
+const API_LIMITER_RULES: ApiLimiterEntry = {
+    "/generate": {
+        "POST": {
+            time: 20,
+            limit: 5
+        }
+    },
+
+    "/is-valid": {
+        "POST": {
+            time: 20,
+            limit: 20,
+        }
+    },
+
+    "/invalidate": {
+        "PATCH": {
+            time: 20,
+            limit: 5
+        }
+    },
+
+    "/verify-owner": {
+        "POST": {
+            time: 20,
+            limit: 20
+        }
+    }
+}
+codesRoute.use(apiLimiter(API_LIMITER_RULES, limitStorage));
+
 
 codesRoute.post("/generate", generateCodeFor);
 
