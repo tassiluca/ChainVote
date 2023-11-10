ff58f45c6269ef484898d493adaee0ac3666688d
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2023-11-10 11:53:11 +0100

    style(auth): Rendering on app startup

--- api-service/auth/src/app.ts
+++ api-service/auth/src/app.ts
@@ -3,5 +3,18 @@ import ExpressConfig from "./configs/express.config";
 const app = ExpressConfig();
 const PORT = process.env.PORT || 8180;
 
-app.listen(PORT, () => console.log("Server Running on Port " + PORT));
-export default app;
+function printInformations() {
+    console.log(`    
+    __  __ __   ____  ____  ____   __ __   ___   ______    ___       ____  __ __  ______  __ __ 
+   /  ]|  T  T /    Tl    j|    \\ |  T  | /   \\ |      T  /  _]     /    T|  T  T|      T|  T  T
+  /  / |  l  |Y  o  | |  T |  _  Y|  |  |Y     Y|      | /  [_     Y  o  ||  |  ||      ||  l  |
+ /  /  |  _  ||     | |  | |  |  ||  |  ||  O  |l_j  l_jY    _]    |     ||  |  |l_j  l_j|  _  |
+/   \\_ |  |  ||  _  | |  | |  |  |l  :  !|     |  |  |  |   [_     |  _  ||  :  |  |  |  |  |  |
+\\     ||  |  ||  |  | j  l |  |  | \\   / l     !  |  |  |     T    |  |  |l     |  |  |  |  |  |
+ \\____jl__j__jl__j__j|____jl__j__j  \\_/   \\___/   l__j  l_____j    l__j__j \\__,_j  l__j  l__j__j
+ `);
+    console.log("DS Project - 2022/2023");
+    console.log("Authors: Giovanni Antonioni, Luca Tassinari, Luca Rubboli");
+    console.log("Server Running on Port " + PORT);
+}
+app.listen(PORT, printInformations);
