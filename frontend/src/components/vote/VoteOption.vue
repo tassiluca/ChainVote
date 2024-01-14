cc6a95aac01939e5aff654dd6a738ee0c663761b
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-14 16:38:27 +0100

    feat: add vote page skeleton

---
+++ frontend/src/components/vote/VoteOption.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+    optId: string;
+    name: string;
+}>()
+
+const model = defineModel()
+</script>
+
+<template>
+    <input type="radio" :id="optId" :name="name" :value="name" v-model="model" />
+    <label :for="optId">{{name}}</label>
+</template>
+
+
---
+++ frontend/src/views/VoteView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import { ref } from "vue";
+    import VoteOption from "@/components/vote/VoteOption.vue";
+
+    const voteOptions = [{ id: 1, name: "Option 1" }, { id: 2, name: "Option 2" }];
+    const voteCode = ref("");
+    const choosedOption = ref("");
+
+    function submitForm(): void {
+        alert(choosedOption.value);
+    }
+    
+</script>
+
+<template>
+    
+    <h1> Prova </h1>
+    <form @submit.prevent="submitForm" method="POST">
+        <input type="text" v-model.lazy="voteCode" />
+        <VoteOption v-for="option in voteOptions" 
+                    :key="option.id" 
+                    :optId="option.id.toString()" 
+                    :name="option.name" 
+                    v-model="choosedOption"/>
+            
+        <button type="submit">Submit</button>
+    </form>
+</template>
