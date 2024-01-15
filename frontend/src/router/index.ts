f797bc0431821e8fbd53edc15a39af6eaa155666
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-01-15 16:53:22 +0100

    fix: import paths

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,13 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'dashboard',
+      component: () => import('@/views/Dashboard.vue')
+    },
+    {
+      path: '/home',
       name: 'home',
-      component: HomeView
+      component: () => import('@/views/HomeView.vue')
     },
     {
       path: '/about',
@@ -15,7 +19,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('@/views/AboutView.vue')
     }
   ]
 })
