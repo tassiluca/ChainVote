439c32ea8763582839e8ded89c7596a89cf8b582
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-01-15 16:51:58 +0100

    chore: add carousel to display elections in dashboard

---
+++ frontend/src/components/ElectionCard.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">{{ election.name }}</h5>
+      <p class="card-text">
+        <ul class="election-props">
+          <li>
+            <strong>Start:</strong> {{ election.start }}
+          </li>
+          <li>
+            <strong>End:</strong> {{ election.end }}
+          </li>
+          <li>
+            <strong>Affluence:</strong> {{ election.affluence }}
+          </li>
+        </ul>
+      </p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineProps, toRefs} from 'vue'
+
+interface Election {
+  id: string,
+  name: string,
+  start: Date,
+  end: Date,
+  affluence: number,
+}
+
+const props = defineProps<{
+  cardId: string,
+  election: Election,
+}>()
+
+const { election } = toRefs(props);
+
+export default {
+  name: "ElectionCardComponent",
+  data() {
+    return {
+      cardId: 'prova',
+      election: {
+        id: 'prova',
+        name: 'prova1',
+        start: new Date('01/03/23'),
+        end: new Date('01/03/23'),
+        affluence: 22,
+      },
+    }
+  },
+};
+</script>
+
+<style>
+  .election-props {
+    list-style-type: none;
+    padding: 0;
+  }
+</style>
---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="elections">
+    <h2>Open Elections</h2>
+    <div class="col-10 center mx-auto election election-open">
+      <Carousel elections=""/>
+    </div>
+  </div>
+  <div class="elections">
+    <h2>Closed Elections</h2>
+    <div class="col-10 center mx-auto election election-closed">
+      <Carousel elections=""/>
+    </div>
+  </div>
+  <div class="elections">
+    <h2>Elections soon...</h2>
+    <div class="col-10 center mx-auto election election-soon">
+      <Carousel elections=""/>
+    </div>
+  </div>
+</template>
+
+<style>
+  body {
+    align-items: center;
+  }
+
+  .elections {
+    text-align: center;
+    margin: 4% 0;
+  }
+
+  .election {
+    margin: 2% 0;
+    border-radius: 15px;
+    box-shadow: 1px 3px 10px rgba(200, 200, 200, 0.82);
+    padding: 2%;
+    background-color: grey;
+  }
+
+  .election-open {
+    border: green 2px solid;
+  }
+
+  .election-closed {
+    border: red 2px solid;
+  }
+
+  .election-soon {
+    border: blue 2px solid;
+  }
+</style>
+
+<script setup lang="ts">
+import Carousel from "@/components/Carousel.vue";
+</script>
