91e75ad79e1fe34bb31f95dd4e7d2b4f7b8e464a
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-16 23:44:54 +0100

    refactor: change headers configuration on api-server

--- api-service/api/src/configs/express.config.ts
+++ api-service/api/src/configs/express.config.ts
@@ -24,6 +24,13 @@ const ExpressConfig = (): Application => {
   app.use(express.json());
   app.use(bodyParser.json());
 
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, PATCH");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  })
+
   // Routes initialization
   app.use("/users", userRouter);
   app.use("/election", electionRouter);
