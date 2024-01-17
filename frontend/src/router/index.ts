25443ea9470e6a4bae819101ccf4237679bc7a62
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-01-17 17:01:44 +0100

    chore: add userArea route with parametric userId, using props

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +20,17 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('@/views/AboutView.vue')
-    }
+    },
+    {
+      path: '/user/:id',
+      name: 'user-area',
+      component: () => import('@/views/UserArea.vue'),
+      props(to) {
+        return {
+          id: to.params.id,
+        }
+      },
+    },
   ]
 })
 
