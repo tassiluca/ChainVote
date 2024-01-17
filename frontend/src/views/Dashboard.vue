48bc90adf99d7aa8233da9b9e96a611bd623fcce
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-01-17 17:06:27 +0100

    refactor: refactored style

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,19 +2,19 @@
   <div class="elections">
     <div class="col-10 center mx-auto election election-open bg-primary">
       <a href="#" class="election-link">Open Elections</a>
-      <Carousel :elections="elections"/>
+      <carousel :elections="elections"/>
     </div>
   </div>
   <div class="elections">
     <div class="col-10 center mx-auto election election-closed bg-primary">
       <a href="#" class="election-link">Closed Elections</a>
-      <Carousel :elections="elections"/>
+      <carousel :elections="elections"/>
     </div>
   </div>
   <div class="elections">
     <div class="col-10 center mx-auto election election-soon bg-primary">
       <a href="#" class="election-link">Elections soon</a>
-      <Carousel :elections="elections"/>
+      <carousel :elections="elections"/>
     </div>
   </div>
 </template>
@@ -23,12 +23,12 @@
 import Carousel from "@/components/Carousel.vue";
 
 const elections = [
-  {id: 'prova1', name: 'prova1', start: new Date('01/03/23'), end: new Date('03/03/23'), affluence: 2201,},
-  {id: 'prova2', name: 'prova2', start: new Date('02/03/23'), end: new Date('04/03/23'), affluence: 25,},
-  {id: 'prova3', name: 'prova3', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: 2200,},
-  {id: 'prova4', name: 'prova4', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: 2200,},
-  //{id: 'prova5', name: 'prova5', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: 2200,},
-  //{id: 'prova6', name: 'prova6', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: 2200,},
+  {id: 'prova1', name: 'prova1', start: new Date('01/03/23'), end: new Date('03/03/23'), affluence: "20%",},
+  {id: 'prova2', name: 'prova2', start: new Date('02/03/23'), end: new Date('04/03/23'), affluence: "20%",},
+  {id: 'prova3', name: 'prova3', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: "20%",},
+  {id: 'prova4', name: 'prova4', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: "20%",},
+  {id: 'prova5', name: 'prova5', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: "20%",},
+  {id: 'prova6', name: 'prova6', start: new Date('03/03/23'), end: new Date('05/03/23'), affluence: "20%",},
 ]
 </script>
 
@@ -53,14 +53,9 @@ const elections = [
     border-radius: 15px;
     box-shadow: 1px 3px 10px rgba(200, 200, 200, 0.82);
     padding: 2%;
-  }
-
-  .election-closed {
-    border: red 2px solid;
-  }
-
-  .election-soon {
-    border: blue 2px solid;
+    button {
+      color: white;
+    }
   }
 </style>
 
@@ -71,6 +66,9 @@ const elections = [
     a:hover {
       color: $color-open;
     }
+    button:hover {
+      color: $color-open;
+    }
   }
   $color-closed: red;
   .election-closed {
@@ -78,11 +76,17 @@ const elections = [
     a:hover {
       color: $color-closed;
     }
+    button:hover {
+      color: $color-closed;
+    }
   }
   $color-soon: blue;
   .election-soon {
     border: $color-soon 2px solid;
     a:hover {
+      color: $color-soon;
+    }
+    button:hover {
       color: $color-soon;
     }
   }
