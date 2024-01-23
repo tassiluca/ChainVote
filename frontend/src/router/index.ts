244df3783446685c1a743b33109370db0c6329bf
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-01-23 15:06:31 +0100

    chore: add ElectionView

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
-import { makeRequest } from '@/assets/utils'
-import { createRouter, createWebHistory } from 'vue-router'
+import {makeRequest} from '@/assets/utils'
+import {createRouter, createWebHistory} from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
 import NotificationsView from '@/views/NotificationsView.vue'
 import VotingDetails from '@/views/VotingDetails.vue'
@@ -35,6 +35,42 @@ const router = createRouter({
         }
       },
       component: () => import('@/views/VoteView.vue'),
+    },
+    {
+      path: '/elections/:qualifier',
+      name: 'elections conditional',
+      beforeEnter: (to, from, next) => {
+        try {
+          if (to.params.qualifier === "open" || to.params.qualifier === "closed" || to.params.qualifier === "soon") {
+            to.meta.qualifier = to.params.qualifier;
+            const jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiX2lkIjoiNjVhNWM0NDE2YTNmN2ZhMTUzYzU0ZjY0IiwiZW1haWwiOiJnaW9yaS5wcm92YUB0ZXN0Lml0IiwicGFzc3dvcmQiOiIkMmIkMTAkYTBOQVRNRjdiaDBIbXlZL0Vrd1lTT0ZkZU12dFZ1RmZYV2VTcGtOdW1JaWxXN2pNL0I5TzIiLCJmaXJzdE5hbWUiOiJHaW9wYWluIiwic2Vjb25kTmFtZSI6Ik5vR2FpbiIsInJvbGUiOiJ1c2VyIiwiX192IjowfSwiaWF0IjoxNzA1NDQyNzcwLCJleHAiOjE3MDU0NDM2NzAsImF1ZCI6Imh0dHBzOi8vd3d3LmNoYWludm90ZS5jb20iLCJpc3MiOiJDaGFpblZvdGUifQ.PUwOiTQFE6la7Vw1fpqerl6gQRKDj2yPTsp01ED3UhpdXxvzDDQ4Ul07pFvuL2-p3Lmd3MvpxyqMIOGnMSlLc84yKNleEgfd7uIpuRAgQ5wi-bAF-XXeEMfHgP9wdw3V_a9ufQTMLrZ2wq3ieyxMrJaRBTThVbJx5NuSUYzTnybMu7p6zcuomSsWzMGs4L4tchkbLhqPKgYdX1jb39wUX3SIxwW-FWShTHO34iKy-BM_ixHkj8ry-_cCt2KZ4eoT1SvYyym-gvK_g0wULy7gLJ66MKiZB5jcgtg77Tan3EnVTvdf__LmMbVIjP55vwKzpj9vzCK3gsznCgINuTaHLlbrDMPJrxcyEOYytwTp6DcuDOFe1-8kKwnwQPy6Sm-A3IZaOG4J6sYyk4NOwzxQI0v2HVwWyT5ON5TCyS_PU2ghlaTbrQnVL0lhwOmtr09V9aaBYNhZvGUs7LEq8N4NHxWcUqsxVivqqAefO-7uY4q10-SlEGUgGCPu1YLT6A2KcpEOejeiRS--hJOiO1LnzuX_prsAhE8NiIRij23zpqxe8XWPwJRX2YnHt-8frDZtTpSnu8sDlXcBu-7CI6EctmuwP3TPyJCyqHfIS5yT_qGT-gEGOr5zZkCR12oT-hXg5JvlbAPgooj8chHZI0IUXBreKG_zZQ3pD5iXZnAHR-E";
+            makeRequest(`http://localhost:8080/elections`, "GET", null, jwtToken)
+                .then((response) => {
+                  to.meta.data = response.data.data;
+                  next();
+                })
+                .catch((error) => {
+                  console.log(error);
+                  to.meta.data = [{
+                    id: "615a5c4416a3f7fa153c54f6",
+                    name: "Election 1",
+                    start: "2021-10-04T00:00:00.000Z",
+                    end: "2021-10-05T00:00:00.000Z",
+                    choices: [
+                      "choice 0",
+                      "choice 1",
+                    ]
+                  }]
+                  next();
+                });
+          } else {
+            next({ name: 'not-found' });
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      component: () => import('@/views/ElectionsConditionalView.vue'),
     },
     {
       path: '/login',
