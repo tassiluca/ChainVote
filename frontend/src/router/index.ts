d7a5f1e2c1ca2cf9f30ad4601f2de22d7ed24953
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-01-24 17:08:29 +0100

    chore: add query params to electionview

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -15,7 +15,56 @@ const router = createRouter({
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: () => import('@/views/Dashboard.vue'),
+      beforeEnter: (to, from, next) => {
+        try {
+          const jwtToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsiX2lkIjoiNjVhNWM0NDE2YTNmN2ZhMTUzYzU0ZjY0IiwiZW1haWwiOiJnaW9yaS5wcm92YUB0ZXN0Lml0IiwicGFzc3dvcmQiOiIkMmIkMTAkYTBOQVRNRjdiaDBIbXlZL0Vrd1lTT0ZkZU12dFZ1RmZYV2VTcGtOdW1JaWxXN2pNL0I5TzIiLCJmaXJzdE5hbWUiOiJHaW9wYWluIiwic2Vjb25kTmFtZSI6Ik5vR2FpbiIsInJvbGUiOiJ1c2VyIiwiX192IjowfSwiaWF0IjoxNzA1NDQyNzcwLCJleHAiOjE3MDU0NDM2NzAsImF1ZCI6Imh0dHBzOi8vd3d3LmNoYWludm90ZS5jb20iLCJpc3MiOiJDaGFpblZvdGUifQ.PUwOiTQFE6la7Vw1fpqerl6gQRKDj2yPTsp01ED3UhpdXxvzDDQ4Ul07pFvuL2-p3Lmd3MvpxyqMIOGnMSlLc84yKNleEgfd7uIpuRAgQ5wi-bAF-XXeEMfHgP9wdw3V_a9ufQTMLrZ2wq3ieyxMrJaRBTThVbJx5NuSUYzTnybMu7p6zcuomSsWzMGs4L4tchkbLhqPKgYdX1jb39wUX3SIxwW-FWShTHO34iKy-BM_ixHkj8ry-_cCt2KZ4eoT1SvYyym-gvK_g0wULy7gLJ66MKiZB5jcgtg77Tan3EnVTvdf__LmMbVIjP55vwKzpj9vzCK3gsznCgINuTaHLlbrDMPJrxcyEOYytwTp6DcuDOFe1-8kKwnwQPy6Sm-A3IZaOG4J6sYyk4NOwzxQI0v2HVwWyT5ON5TCyS_PU2ghlaTbrQnVL0lhwOmtr09V9aaBYNhZvGUs7LEq8N4NHxWcUqsxVivqqAefO-7uY4q10-SlEGUgGCPu1YLT6A2KcpEOejeiRS--hJOiO1LnzuX_prsAhE8NiIRij23zpqxe8XWPwJRX2YnHt-8frDZtTpSnu8sDlXcBu-7CI6EctmuwP3TPyJCyqHfIS5yT_qGT-gEGOr5zZkCR12oT-hXg5JvlbAPgooj8chHZI0IUXBreKG_zZQ3pD5iXZnAHR-E";
+          makeRequest(`http://localhost:8080/elections`, "GET", null, jwtToken)
+            .then((response) => {
+              to.meta.data = response.data.data;
+              next();
+            })
+            .catch((error) => {
+              to.meta.data = [{
+                id: "id1",
+                name: "Elezione del presidente del consiglio dei ministri prova 1",
+                start: new Date("2021-10-04T10:00"),
+                end: new Date("2026-11-04T10:00"),
+                affluence: "20%",
+                choices: [
+                  "choice 0",
+                  "choice 1",
+                ]
+              },
+                {
+                  id: "id2",
+                  name: "Election 2",
+                  start: new Date("2021-10-04T10:00"),
+                  end: new Date("2025-11-04T10:00"),
+                  affluence: "20%",
+                  choices: [
+                    "choice 0",
+                    "choice 1",
+                  ]
+                },
+                {
+                  id: "id3",
+                  name: "Election 3",
+                  start: new Date("2027-10-04T10:00"),
+                  end: new Date("2028-11-04T10:00"),
+                  affluence: "20%",
+                  choices: [
+                    "choice 0",
+                    "choice 1",
+                  ]
+                }
+              ]
+              next();
+            });
+        } catch (error) {
+          console.log(error);
+        }
+      },
+      component: () => import('@/views/DashboardView.vue'),
     },
     {
       path: '/vote/:id',
@@ -38,7 +87,8 @@ const router = createRouter({
     },
     {
       path: '/elections',
-      name: 'elections-conditional',
+      name: 'elections',
+      props: route => ({ qualifier: route.query.qualifier }),
       beforeEnter: (to, from, next) => {
         try {
           if (to.params.qualifier === undefined) {
@@ -59,8 +109,8 @@ const router = createRouter({
                   to.meta.data = [{
                       id: "id1",
                       name: "Elezione del presidente del consiglio dei ministri prova 1",
-                      start: new Date("2021-10-04T10:00"),
-                      end: new Date("2021-11-04T10:00"),
+                      start: new Date("October 13, 2022 10:00"),
+                      end: new Date("October 13, 2025 10:00"),
                       status: "open",
                       affluence: "20%",
                       choices: [
@@ -170,7 +220,7 @@ const router = createRouter({
           next();
         }
       },
-      component: () => import('@/views/UserArea.vue'),
+      component: () => import('@/views/UserAreaView.vue'),
     },
     {
       path: '/register', 
