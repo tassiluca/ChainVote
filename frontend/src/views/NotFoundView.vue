48d9438a44a187126ee3ac2d1f526a940db75f82
Luca Rubboli <luca.rubboli2@studio.unibo.it>
2024-01-24 23:12:47 +0100

    chore: add error views

---
+++ frontend/src/views/NotFoundView.vue
@@ -0,0 +1,18 @@
+<template>
+  <Breadcrumb :paths="[]" />
+  <PageTitle title="Error" />
+  <div class="text-center err">
+    Oops, seems the system is not able to find what you're looking for, try again please.
+  </div>
+</template>
+
+<script setup lang="ts">
+import Breadcrumb from "@/components/BreadcrumbComponent.vue";
+import PageTitle from "@/components/PageTitleComponent.vue";
+</script>
+
+<style scoped>
+div.err {
+  font-size: x-large;
+}
+</style>
