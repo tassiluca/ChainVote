466afffe295d53a015d6599b4f9891aeae1c7cc9
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2024-01-31 22:27:54 +0100

    refactor: add password from secrets

--- api-service/api/src/configs/mailer.config.ts
+++ api-service/api/src/configs/mailer.config.ts
@@ -1,13 +1,21 @@
 import nodemailer from 'nodemailer';
+import fs from "fs";
 
+const path = "/run/secrets/google_api_secret";
 let secretValue = "";
 
-secretValue= "vmck idvj ewbi etzk";
+fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+        console.error(`Error reading secret file: ${err.message}`);
+        process.exit(1);
+    }
+    secretValue = data.trim();
+});
 
 const config = {
     service: 'gmail',
     auth: {
-        user: "antonioni.giovanni9@gmail.com",
+        user: process.env.MAIL_USER,
         pass: secretValue
     }
 }
